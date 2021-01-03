9cbfe63a837b89771ca7c682ded638e595644a79
Aldrin Cuvi <aldrincuvi@hotmail.com>
2021-01-02 22:14:37 -0500

    Add button functionality.

--- public/js/log.js
+++ public/js/log.js
@@ -1,6 +1,13 @@
+//Botones para interacción entre sign in y sign up.
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
+
+//Boton para ingresar.
+const signin = document.querySelector("#signin");
+//Boton para registarse.
+const signup = document.querySelector("#signup");
+
 
 sign_up_btn.addEventListener('click', () => {
     container.classList.add("sign-up-mode");
@@ -9,3 +16,11 @@ sign_up_btn.addEventListener('click', () => {
 sign_in_btn.addEventListener('click', () => {
     container.classList.remove("sign-up-mode");
 });
+
+signin.addEventListener('click', () => {
+    location.replace("order.html")
+}); 
+
+signup.addEventListener('click', () => {
+    location.replace("new_order.html")
+}); 
